cba559dda68097d99651d451efe07035333d9391
Ngọc Diệp <61446841+alietran@users.noreply.github.com>
2021-08-04 19:31:46 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import OrderBurger from "./OrderBurger/OrderBurger";
+
+
+export default class App extends Component {
+  render() {
+    return <OrderBurger />;
+   
+    
+    
+  }
+}
